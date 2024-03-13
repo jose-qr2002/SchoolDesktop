d00466812e4b6d14939e03378825fd806d0494e3
Jose Quispe <jkqr2016@gmail.com>
2024-03-12 21:10:50 -0500

    Agregando mas propiedades al instructor

--- WindowsFormsApp3/common/instructor/EntidadInstructor.cs
+++ WindowsFormsApp3/common/instructor/EntidadInstructor.cs
@@ -8,10 +8,19 @@ namespace WindowsFormsApp3.common.instructor
 {
     public class EntidadInstructor
     {
+        public int Id { get; set; }
         public String Dni { get; set; }
         public String Nombres { get; set; }
         public String Apellidos { get; set; }
-        public int Id { get; set; }
+        public String Telefono {  get; set; }
+        public String Direccion {  get; set; }
+        public DateTime FechaNac { get; set; }
+        public String Sexo { get; set; }
+        public String Especialidad { get; set; }
+        public String Titulo { get; set; }
+        public Double Salario { get; set; }
+        public DateTime FechaContrato { get; set; }
+        public String EstadoEmpleo { get; set; }
 
         // Constructor
         public EntidadInstructor(string dni, string nombres, string apellidos)
