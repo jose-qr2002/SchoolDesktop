26d6e07467f92c42a954b379e87992fa9bb17b30
Jose Quispe <jkqr2016@gmail.com>
2024-03-18 15:57:19 -0500

    Agregando la entidad Curso

---
+++ WindowsFormsApp3/common/curso/EntidadCurso.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.common.curso
+{
+    public class EntidadCurso
+    {
+        public int Id { get; set; }
+        public String Codigo { get; set; }
+        public String Nombre { get; set; }
+        public String Ciclo { get; set; }
+        public String Carrera { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFinalizacion { get; set; }
+    
+        // Constructor adecuado para insertar un nuevo registro a la base de datos
+        public EntidadCurso(string codigo, string nombre, string ciclo, string carrera, DateTime fechaInicio, DateTime fechaFinalizacion)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Ciclo = ciclo;
+            Carrera = carrera;
+            FechaInicio = fechaInicio;
+            FechaFinalizacion = fechaFinalizacion;
+        }
+
+        // Constructor por defecto
+        public EntidadCurso() { }
+    }
+}
