663f09c8af2b6d8c2001e39e960e49f1b05019eb
Jose Quispe <jkqr2016@gmail.com>
2024-03-18 19:15:15 -0500

    Agregando validacion para Ingresar un Curso

--- WindowsFormsApp3/negocio/curso/NegocioCurso.cs
+++ WindowsFormsApp3/negocio/curso/NegocioCurso.cs
@@ -16,6 +16,37 @@ namespace WindowsFormsApp3.negocio.curso
 
         public int InsertarCursoN(EntidadCurso curso)
         {
+            // Validaciones
+            if (curso.Nombre == "") {
+                MessageBox.Show("El campo nombre esta vacio");
+                return 0;
+            }
+            if (curso.Carrera == "")
+            {
+                MessageBox.Show("El campo carrera esta vacio");
+                return 0;
+            }
+            if (curso.Ciclo == "")
+            {
+                MessageBox.Show("El campo ciclo esta vacio");
+                return 0;
+            }
+
+            string[] ciclos = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
+            List<string> listaCiclos = new List<string>(ciclos);
+
+            if (!listaCiclos.Contains(curso.Ciclo))
+            {
+                MessageBox.Show("Escoja un ciclo válido");
+                return 0;
+            }
+            if (curso.FechaInicio > curso.FechaFinalizacion)
+            {
+                MessageBox.Show("La fecha de Inicio no puede ser posterior a la de Finalización del curso");
+                return 0;
+            }
+
+
             int numRes = _datosCurso.InsertarCurso(curso);
             return numRes;
         }
