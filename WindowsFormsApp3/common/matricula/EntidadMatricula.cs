5551312c9c6a40add67a0c847ad658612e681600
Jose Quispe <jkqr2016@gmail.com>
2024-03-25 18:46:21 -0500

    Agregando la entidad Matricula con sus propiedades

---
+++ WindowsFormsApp3/common/matricula/EntidadMatricula.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.common.matricula
+{
+    public class EntidadMatricula
+    {
+        private int Id { get; set; }
+        private int IdAlumno { get; set; }
+        private int IdCurso { get; set; }
+        private string AnioAcademico { get; set; }
+
+        public EntidadMatricula() { }
+    }
+}
