c4cd9e2dca600ab6eb433528454a91052bc88f9a
Jose Quispe <jkqr2016@gmail.com>
2024-03-26 23:28:14 -0500

    Comunicando la capa de negocio con la de datos de Matricula

--- WindowsFormsApp3/datos/matricula/DatosMatricula.cs
+++ WindowsFormsApp3/datos/matricula/DatosMatricula.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using SenatiPractica;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp3.datos.matricula
 {
-    internal class DatosMatricula
+    public class DatosMatricula
     {
+        public DataTable ObtenerTodosMatriculas()
+        {
+
+            try
+            {
+
+                using (SqlCommand cmd = new SqlCommand("obtenerTodasMatriculas", Connection.Singleton.SqlConnetionFactory))
+                {
+                    DataTable dtData = new DataTable();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataAdapter sqlSda = new SqlDataAdapter(cmd);
+                    sqlSda.Fill(dtData);
+                    return dtData;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+        }
     }
 }
---
+++ WindowsFormsApp3/negocio/matricula/NegocioMatricula.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp3.datos.instructor;
+using WindowsFormsApp3.datos.matricula;
+
+namespace WindowsFormsApp3.negocio.matricula
+{
+    public class NegocioMatricula
+    {
+        public DatosMatricula _datosMatricula = new DatosMatricula();
+        public DataTable ObtenerTodasMatriculasN()
+        {
+
+            return _datosMatricula.ObtenerTodosMatriculas();
+        }
+    }
+}
