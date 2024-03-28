b8232d32d40adde5ca6f3a90eb3ad933189c5b19
Jose Quispe <jkqr2016@gmail.com>
2024-03-27 23:39:29 -0500

    Añadiendo campos para buscar curso en el formulario Matricula

--- WindowsFormsApp3/presentacion/matricula/FrmMenuMatricula.Designer.cs
+++ WindowsFormsApp3/presentacion/matricula/FrmMenuMatricula.Designer.cs
@@ -38,6 +38,12 @@
             this.lblNombres = new System.Windows.Forms.Label();
             this.btnMatricula = new System.Windows.Forms.Button();
             this.lblApellidos = new System.Windows.Forms.Label();
+            this.lblBusquedaCursoTitulo = new System.Windows.Forms.Label();
+            this.txtBusquedaCurso = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTipoBusquedaCurso = new System.Windows.Forms.ComboBox();
+            this.btnBuscarCurso = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dvgMatriculados)).BeginInit();
             this.SuspendLayout();
             // 
@@ -62,7 +68,7 @@
             // 
             this.lblBusquedaDniTitulo.AutoSize = true;
             this.lblBusquedaDniTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBusquedaDniTitulo.Location = new System.Drawing.Point(66, 18);
+            this.lblBusquedaDniTitulo.Location = new System.Drawing.Point(64, 18);
             this.lblBusquedaDniTitulo.Name = "lblBusquedaDniTitulo";
             this.lblBusquedaDniTitulo.Size = new System.Drawing.Size(166, 25);
             this.lblBusquedaDniTitulo.TabIndex = 2;
@@ -72,7 +78,7 @@
             // 
             this.lblDatosAlumnoTitulo.AutoSize = true;
             this.lblDatosAlumnoTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDatosAlumnoTitulo.Location = new System.Drawing.Point(303, 22);
+            this.lblDatosAlumnoTitulo.Location = new System.Drawing.Point(274, 22);
             this.lblDatosAlumnoTitulo.Name = "lblDatosAlumnoTitulo";
             this.lblDatosAlumnoTitulo.Size = new System.Drawing.Size(151, 22);
             this.lblDatosAlumnoTitulo.TabIndex = 4;
@@ -84,19 +90,19 @@
             this.dvgMatriculados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dvgMatriculados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dvgMatriculados.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.dvgMatriculados.Location = new System.Drawing.Point(42, 129);
+            this.dvgMatriculados.Location = new System.Drawing.Point(42, 170);
             this.dvgMatriculados.Margin = new System.Windows.Forms.Padding(2);
             this.dvgMatriculados.MultiSelect = false;
             this.dvgMatriculados.Name = "dvgMatriculados";
             this.dvgMatriculados.RowTemplate.Height = 24;
             this.dvgMatriculados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dvgMatriculados.Size = new System.Drawing.Size(614, 220);
+            this.dvgMatriculados.Size = new System.Drawing.Size(779, 220);
             this.dvgMatriculados.TabIndex = 8;
             // 
             // lblNombresEtiqueta
             // 
             this.lblNombresEtiqueta.AutoSize = true;
-            this.lblNombresEtiqueta.Location = new System.Drawing.Point(304, 57);
+            this.lblNombresEtiqueta.Location = new System.Drawing.Point(275, 57);
             this.lblNombresEtiqueta.Name = "lblNombresEtiqueta";
             this.lblNombresEtiqueta.Size = new System.Drawing.Size(52, 13);
             this.lblNombresEtiqueta.TabIndex = 9;
@@ -105,7 +111,7 @@
             // lblApellidosEtiqueta
             // 
             this.lblApellidosEtiqueta.AutoSize = true;
-            this.lblApellidosEtiqueta.Location = new System.Drawing.Point(304, 82);
+            this.lblApellidosEtiqueta.Location = new System.Drawing.Point(275, 82);
             this.lblApellidosEtiqueta.Name = "lblApellidosEtiqueta";
             this.lblApellidosEtiqueta.Size = new System.Drawing.Size(52, 13);
             this.lblApellidosEtiqueta.TabIndex = 10;
@@ -114,7 +120,7 @@
             // lblNombres
             // 
             this.lblNombres.AutoSize = true;
-            this.lblNombres.Location = new System.Drawing.Point(358, 57);
+            this.lblNombres.Location = new System.Drawing.Point(329, 57);
             this.lblNombres.Name = "lblNombres";
             this.lblNombres.Size = new System.Drawing.Size(27, 13);
             this.lblNombres.TabIndex = 11;
@@ -122,7 +128,7 @@
             // 
             // btnMatricula
             // 
-            this.btnMatricula.Location = new System.Drawing.Point(565, 47);
+            this.btnMatricula.Location = new System.Drawing.Point(746, 47);
             this.btnMatricula.Name = "btnMatricula";
             this.btnMatricula.Size = new System.Drawing.Size(75, 47);
             this.btnMatricula.TabIndex = 12;
@@ -132,17 +138,78 @@
             // lblApellidos
             // 
             this.lblApellidos.AutoSize = true;
-            this.lblApellidos.Location = new System.Drawing.Point(358, 81);
+            this.lblApellidos.Location = new System.Drawing.Point(329, 81);
             this.lblApellidos.Name = "lblApellidos";
             this.lblApellidos.Size = new System.Drawing.Size(27, 13);
             this.lblApellidos.TabIndex = 13;
             this.lblApellidos.Text = "N/A";
             // 
+            // lblBusquedaCursoTitulo
+            // 
+            this.lblBusquedaCursoTitulo.AutoSize = true;
+            this.lblBusquedaCursoTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBusquedaCursoTitulo.Location = new System.Drawing.Point(466, 19);
+            this.lblBusquedaCursoTitulo.Name = "lblBusquedaCursoTitulo";
+            this.lblBusquedaCursoTitulo.Size = new System.Drawing.Size(186, 25);
+            this.lblBusquedaCursoTitulo.TabIndex = 14;
+            this.lblBusquedaCursoTitulo.Text = "Busqueda de Curso";
+            // 
+            // txtBusquedaCurso
+            // 
+            this.txtBusquedaCurso.Location = new System.Drawing.Point(471, 55);
+            this.txtBusquedaCurso.Name = "txtBusquedaCurso";
+            this.txtBusquedaCurso.Size = new System.Drawing.Size(108, 20);
+            this.txtBusquedaCurso.TabIndex = 15;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(470, 82);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 16;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(524, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "N/A";
+            // 
+            // cmbTipoBusquedaCurso
+            // 
+            this.cmbTipoBusquedaCurso.FormattingEnabled = true;
+            this.cmbTipoBusquedaCurso.Items.AddRange(new object[] {
+            "CODIGO",
+            "NOMBRE"});
+            this.cmbTipoBusquedaCurso.Location = new System.Drawing.Point(594, 54);
+            this.cmbTipoBusquedaCurso.Name = "cmbTipoBusquedaCurso";
+            this.cmbTipoBusquedaCurso.Size = new System.Drawing.Size(104, 21);
+            this.cmbTipoBusquedaCurso.TabIndex = 18;
+            // 
+            // btnBuscarCurso
+            // 
+            this.btnBuscarCurso.Location = new System.Drawing.Point(471, 109);
+            this.btnBuscarCurso.Name = "btnBuscarCurso";
+            this.btnBuscarCurso.Size = new System.Drawing.Size(108, 23);
+            this.btnBuscarCurso.TabIndex = 19;
+            this.btnBuscarCurso.Text = "Buscar";
+            this.btnBuscarCurso.UseVisualStyleBackColor = true;
+            // 
             // FrmMenuMatricula
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 380);
+            this.ClientSize = new System.Drawing.Size(855, 415);
+            this.Controls.Add(this.btnBuscarCurso);
+            this.Controls.Add(this.cmbTipoBusquedaCurso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtBusquedaCurso);
+            this.Controls.Add(this.lblBusquedaCursoTitulo);
             this.Controls.Add(this.lblApellidos);
             this.Controls.Add(this.btnMatricula);
             this.Controls.Add(this.lblNombres);
@@ -174,5 +241,11 @@
         private System.Windows.Forms.Label lblNombres;
         private System.Windows.Forms.Button btnMatricula;
         private System.Windows.Forms.Label lblApellidos;
+        private System.Windows.Forms.Label lblBusquedaCursoTitulo;
+        private System.Windows.Forms.TextBox txtBusquedaCurso;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTipoBusquedaCurso;
+        private System.Windows.Forms.Button btnBuscarCurso;
     }
 }
