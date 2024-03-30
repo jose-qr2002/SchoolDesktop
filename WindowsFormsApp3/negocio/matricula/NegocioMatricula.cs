e6ea6e5ddc7e5defa11d676d1d1a7c413d0bbc48
Jose Quispe <jkqr2016@gmail.com>
2024-03-30 00:28:44 -0500

    Añadiendo validaciones al ingresar una nueva Matricula

--- WindowsFormsApp3/negocio/matricula/NegocioMatricula.cs
+++ WindowsFormsApp3/negocio/matricula/NegocioMatricula.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using WindowsFormsApp3.common.matricula;
 using WindowsFormsApp3.datos.instructor;
 using WindowsFormsApp3.datos.matricula;
@@ -21,6 +22,23 @@ namespace WindowsFormsApp3.negocio.matricula
 
         public int InsertarMatriculaN(EntidadMatricula matricula)
         {
+            if(matricula.IdAlumno == 0)
+            {
+                MessageBox.Show("Alumno No Seleccionado");
+                return 0;
+            }
+            if(matricula.IdCurso == 0)
+            {
+                MessageBox.Show("Curso No Seleccionado");
+                return 0;
+            }
+            List<string> aniosAcademicos = new List<string>{"2024-I","2024-II"};
+            if (!aniosAcademicos.Contains(matricula.AnioAcademico))
+            {
+                MessageBox.Show("Año academico no valido");
+                return 0;
+            }
+
             return _datosMatricula.InsertarMatricula(matricula);
         }
     }
