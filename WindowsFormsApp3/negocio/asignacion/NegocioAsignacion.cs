5739b98bfeec8ecb28f79ea46c45230897dcf85e
Jose Quispe <jkqr2016@gmail.com>
2024-04-01 21:06:09 -0500

    Agregando capa de negocio para Asignacion

---
+++ WindowsFormsApp3/negocio/asignacion/NegocioAsignacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp3.common.asignacion;
+using WindowsFormsApp3.datos.asignacion;
+
+namespace WindowsFormsApp3.negocio.asignacion
+{
+    public class NegocioAsignacion
+    {
+        private DatosAsignacion _datosAsignacion = new DatosAsignacion();
+
+        public int insertarAsignacionN(EntidadAsignacion asignacion)
+        {
+            return _datosAsignacion.insertarAsignacion(asignacion);
+        }
+    }
+}
