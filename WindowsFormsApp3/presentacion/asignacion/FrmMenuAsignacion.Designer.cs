7e6768f3243c5679c9ab8eedd158a4aa7085a4a2
Jose Quispe <jkqr2016@gmail.com>
2024-04-01 19:22:22 -0500

    Agregando validacion para buscar Instructor em Asignaciones

--- WindowsFormsApp3/presentacion/asignacion/FrmMenuAsignacion.Designer.cs
+++ WindowsFormsApp3/presentacion/asignacion/FrmMenuAsignacion.Designer.cs
@@ -230,6 +230,7 @@
             this.btnBusquedaDni.TabIndex = 23;
             this.btnBusquedaDni.Text = "Buscar";
             this.btnBusquedaDni.UseVisualStyleBackColor = true;
+            this.btnBusquedaDni.Click += new System.EventHandler(this.btnBusquedaDni_Click);
             // 
             // FrmMenuAsignacion
             // 
--- WindowsFormsApp3/presentacion/asignacion/FrmMenuAsignacion.cs
+++ WindowsFormsApp3/presentacion/asignacion/FrmMenuAsignacion.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp3.common.instructor;
+using WindowsFormsApp3.negocio.instructor;
 
 namespace WindowsFormsApp3.presentacion.asignacion
-{
+{  
     public partial class FrmMenuAsignacion : Form
     {
+        private NegocioInstructor _negocioInstructor = new NegocioInstructor();
+        // IDS
+        private int id_instructor;
+
         public FrmMenuAsignacion()
         {
             InitializeComponent();
+        }
+
+        private void btnBusquedaDni_Click(object sender, EventArgs e)
+        {
+            string parametroDni = txtDni.Text;
+            DataTable instructores = _negocioInstructor.BuscarInstructorByTipoAndParametroN(ETipoBusquedaInstructor.Dni, parametroDni);
+            if (instructores.Rows.Count == 0) 
+            {
+                MessageBox.Show("No se encontraron instructores");
+                lblNombresInstructor.Text = "N/A";
+                lblApellidosInstructor.Text = "N/A";
+                return;
+            }
+            lblNombresInstructor.Text = instructores.Rows[0]["nombre"].ToString();
+            lblApellidosInstructor.Text = instructores.Rows[0]["apellido"].ToString();
+            id_instructor = Int32.Parse(instructores.Rows[0]["id"].ToString());
+            MessageBox.Show("Se encontro un instructor");
         }
     }
 }
