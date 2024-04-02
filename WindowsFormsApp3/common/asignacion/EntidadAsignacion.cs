3df42db43204568927040c42d1d3ce3a2662d425
Jose Quispe <jkqr2016@gmail.com>
2024-04-01 20:42:33 -0500

    Agregnado entidad para Asignacion

---
+++ WindowsFormsApp3/common/asignacion/EntidadAsignacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3.common.asignacion
+{
+    public class EntidadAsignacion
+    {
+        public int Id { get; set; }
+        public int IdCurso { get; set; }
+        public int IdInstructor { get; set; }
+        public string AnioAcademico { get; set; }
+    }
+}
